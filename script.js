0df033812d2707dec2d1632dc227cc9ab9a0a7e8
omrlns <marlon.da.silva996@gmail.com>
2025-05-16 09:06:12 -0300

    arrumando pequenos detalhes

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-let itens = [["Luger","luger.png",200],["M1911","m1911.png",150]];
+let itens = [["Luger", "luger.png", 200], ["M1911", "m1911.png", 150]];
 const header = document.getElementById('header');
 const logo = document.getElementById('logo');
 const logoBg = document.getElementById('logoBg');
 const pageNav = document.getElementById('pageNav');
 
-logo.addEventListener('mousedown',()=>{
+logo.addEventListener('mousedown', () => {
     window.scrollTo(0, 0);
 })
 
-document.addEventListener('scroll', ()=>{
-    if (window.scrollY<3900){
-        if (window.scrollY>=100){
+document.addEventListener('scroll', () => {
+    if (window.scrollY < 3900) {
+        if (window.scrollY >= 100) {
             header.style.opacity = "1";
             logoBg.style.opacity = "1";
-            logo.style.top = String(window.scrollY+110);
-        }else{
+            logo.style.top = String(window.scrollY + 110);
+        } else {
             header.style.opacity = "0";
             logoBg.style.opacity = "1";
-            logo.style.top = String(window.scrollY+110);
+            logo.style.top = String(window.scrollY + 110);
         }
         header.style.top = String(window.scrollY);
-        logoBg.style.top = String(window.scrollY+100);
-        pageNav.style.top = String(window.scrollY+42);
-    }  
+        logoBg.style.top = String(window.scrollY + 100);
+        pageNav.style.top = String(window.scrollY + 42);
+    }
 })
